d8dc7254bba4224c224424d71589741edb1c8fac
Camanrofo <camanrofo@gmail.com>
2026-08-01 18:35:21 -0500

    Spawner + vision + optimization

--- player.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-var speed: float = 150
-
-func	_physics_process(delta: float) -> void:
-	velocity = Input.get_vector("left", "right", "up", "down") * speed
-	move_and_collide(velocity * delta)
-	
-	
